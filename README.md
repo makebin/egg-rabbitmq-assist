0a68d94c83ed65061cad0e28ef60e9343c23a1fe
You <mk9007@163.com>
2024-08-22 15:17:40 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,34 @@
+# egg-rabbitmq-assist
+
+## 依赖说明
+
+### 依赖的 egg 版本
+
+egg-rabbitmq-assist 版本 | egg 1.x
+--- | ---
+1.x | 😁
+0.x | ❌
+
+### 依赖的插件
+<!--
+
+如果有依赖其它插件，请在这里特别说明。如
+
+- security
+- multipart
+
+-->
+
+## 开启插件
+
+```js
+// config/plugin.js
+exports.rabbitmq = {
+  enable: true,
+  package: 'egg-rabbitmq-assist',
+};
+```
+## 
+主要解决npm上没找到，需要git+https://github.com/tsui66/egg-rabbitmq这种方式下载依赖包
+实际包
+https://github.com/tsui66/egg-rabbitmq
---
+++ agent.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const rabbitmq = require('./lib/rabbitmq');
+
+module.exports = agent => {
+  agent.logger.info('[egg-rabbitmq-assist] plugin init');
+
+  const config = agent.config.rabbitmq;
+  if (config.agent) {
+    rabbitmq(agent);
+  }
+
+  class RabbitMQStrategy extends agent.ScheduleStrategy {
+    async start() {
+      const { schedule: { exchange, queue: queueName, worker } } = this;
+      const ch = await agent.rabbitmq.createChannel();
+      await ch.assertExchange(exchange, 'direct', {
+        durable: false,
+      });
+      const _queueName = queueName || agent.config.rabbitmq.queueName;
+      await ch.assertQueue(_queueName, { durable: false });
+      ch.consume(_queueName, msg => {
+        if (worker === 'all') {
+          this.sendAll({ schedule: this.schedule, msg, ackEvent: 'rabbitmq_ack' });
+        } else if (worker === 'one') {
+          this.sendOne({ schedule: this.schedule, msg, ackEvent: 'rabbitmq_ack' });
+        } else {
+          throw new Error(`[egg-rabbitmq-assist] unknow worker type ${worker}`);
+        }
+      });
+      agent.messenger.on('rabbitmq_ack', payload => {
+        if (payload && payload.emitter) {
+          agent.messenger.sendToApp(payload.emitter, payload.data || {});
+        }
+        ch.ack(payload.msg);
+      });
+    }
+  }
+
+  agent.schedule.use('rabbitmq', RabbitMQStrategy);
+};
